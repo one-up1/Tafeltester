734f5cb174b6123a902903b2234e4a1ffc67aaa2
Ferenc Dykstra <oneup1@gmail.com>
2021-01-19 14:57:12 +0100

    - Add equations.

--- Tafeltester/MainWindow.xaml.cs
+++ Tafeltester/MainWindow.xaml.cs
@@ -20,9 +20,37 @@ namespace Tafeltester
     /// </summary>
     public partial class MainWindow : Window
     {
+        private const int EQUATION_COUNT = 5;
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void bAddEquations_Click(object sender, RoutedEventArgs e)
+        {
+            spEquations.Children.Clear();
+
+            //TODO: Validate max string and values < EQUATION_COUNT
+            int maxValue = int.Parse(tbMaxValue.Text);
+
+            List<int> values = new List<int>();
+            for (int i = 1; i <= maxValue; i++)
+            {
+                values.Add(i);
+            }
+
+            Random random = new Random();
+            for (int i = 1; i <= EQUATION_COUNT; i++)
+            {
+                int index = random.Next(0, values.Count);
+                int value = values[index];
+                values.RemoveAt(index);
+
+                Label lEquation = new Label();
+                lEquation.Content = i + " x " + value + " =";
+                spEquations.Children.Add(lEquation);
+            }
         }
     }
 }
