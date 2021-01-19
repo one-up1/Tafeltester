0c2b970902dfae044b6698c6c18e5ddce0cb5f14
Ferenc Dykstra <oneup1@gmail.com>
2021-01-19 15:53:42 +0100

    - Check results.

--- Tafeltester/MainWindow.xaml.cs
+++ Tafeltester/MainWindow.xaml.cs
@@ -30,6 +30,8 @@ namespace Tafeltester
         private void bAddEquations_Click(object sender, RoutedEventArgs e)
         {
             spEquations.Children.Clear();
+            bCheckResults.IsEnabled = false;
+            lScore.Content = "";
 
             //TODO: Validate max string and values < EQUATION_COUNT
             int maxValue = int.Parse(tbMaxValue.Text);
@@ -43,14 +45,68 @@ namespace Tafeltester
             Random random = new Random();
             for (int i = 1; i <= EQUATION_COUNT; i++)
             {
-                int index = random.Next(0, values.Count);
-                int value = values[index];
-                values.RemoveAt(index);
+                int valueIndex = random.Next(0, values.Count);
+                int value = values[valueIndex];
+                values.RemoveAt(valueIndex);
+
+                StackPanel spEquation = new StackPanel();
+                spEquation.Orientation = Orientation.Horizontal;
 
                 Label lEquation = new Label();
                 lEquation.Content = i + " x " + value + " =";
-                spEquations.Children.Add(lEquation);
+                spEquation.Children.Add(lEquation);
+
+                TextBox tbResult = new TextBox();
+                tbResult.Width = 100;
+                tbResult.Tag = i * value;
+                spEquation.Children.Add(tbResult);
+
+                Label lResult = new Label();
+                lResult.FontWeight = FontWeights.Bold;
+                spEquation.Children.Add(lResult);
+
+                spEquations.Children.Add(spEquation);
             }
+
+            bCheckResults.IsEnabled = true;
+        }
+
+        private void bCheckResults_Click(object sender, RoutedEventArgs e)
+        {
+            int correctResults = 0;
+            for (int i = 0; i < EQUATION_COUNT; i++)
+            {
+                StackPanel spEquation = (StackPanel)spEquations.Children[i];
+                TextBox tbResult = (TextBox)spEquation.Children[1];
+
+                int result;
+                try
+                {
+                    result = int.Parse(tbResult.Text);
+                }
+                catch
+                {
+                    lScore.Content = "Vul alle sommen in";
+                    lScore.Foreground = Brushes.Red;
+                    return;
+                }
+
+                Label lResult = (Label)spEquation.Children[2];
+                if (result == (int)tbResult.Tag)
+                {
+                    correctResults++;
+                    lResult.Content = "Goed";
+                    lResult.Foreground = Brushes.Green;
+                }
+                else
+                {
+                    lResult.Content = "Fout";
+                    lResult.Foreground = Brushes.Red;
+                }
+            }
+
+            lScore.Content = "Je hebt een " + Math.Round(correctResults * 10d / EQUATION_COUNT);
+            lScore.Foreground = Brushes.Black;
         }
     }
 }
